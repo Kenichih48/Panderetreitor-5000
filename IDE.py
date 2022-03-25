4f5a401a4cfb94895d9a1fab9eba3c95d8e8f95e
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2022-03-25 00:30:09 -0600

    Bugs corregidos y se agrega el highlight

--- IDE.py
+++ IDE.py
@@ -10,28 +10,60 @@ root = Tk()
 root.geometry("800x600")
 root.title("Tambarduine IDE")
 
-scrollbar = Scrollbar(root)
-scrollbar.pack(side=RIGHT, fill=Y)
 
 main_path = ''
+errors_names = []
+
 
 def comp_run():
-    text_output.configure(state='normal')
-    text_output.delete('1.0', END)
-    text_output.insert('1.0',"Hey! You ran and compiled the code :)")
-    text_output.configure(state='disabled')
+    global errors_names
 
+    
+    for i in errors_names:
+        print("erasing error: "+i)
+        text_lineNum.tag_delete(i)
+    
+    #errorFound([1,10,25])
+    error_list = []
+    if not errorFound(error_list):
+        
+        text_output.configure(state='normal')
+        text_output.delete('1.0', END)
+        text_output.insert('1.0',"Hey! You ran and compiled the code :)")
+        text_output.configure(state='disabled')
+
+    else:
+        text_output.configure(state='normal')
+        text_output.delete('1.0', END)
+        text_output.insert('1.0',"Errors found")
+        text_output.configure(state='disabled')
+
+
+    
 def comp():
+    global errors_names
     #global main_path
     #if main_path == '':
     #    save_warning = Toplevel()
     #    notice = Label(save_warning, text="Please save the file first")
     #    notice.pack()
     #    return
-    text_output.configure(state='normal')
-    text_output.delete('1.0', END)
-    text_output.insert('1.0',"Woah buddy, you've just compiled your code!")
-    text_output.configure(state='disabled')
+
+    for i in errors_names:
+        text_lineNum.tag_delete(i)
+
+    error_list = [0,7,12, 25 ,45]
+    if not errorFound(error_list):
+        text_output.configure(state='normal')
+        text_output.delete('1.0', END)
+        text_output.insert('1.0',"Woah buddy, you've just compiled your code!")
+        text_output.configure(state='disabled')
+    else:
+        text_output.configure(state='normal')
+        text_output.delete('1.0', END)
+        text_output.insert('1.0',"Errors found")
+        text_output.configure(state='disabled')
+
 
 def open_file():
     path = askopenfilename(filetypes=[('Text Files','*.txt')])
@@ -68,10 +100,17 @@ def save_as():
 def multipleview(*args):
     text_info.yview(*args)
     text_lineNum.yview(*args)
+    print(*args)
+
+
+
+def on_mousewheel(event):
+    return 'break'
+
+
 
 canvas = Canvas(root)
 canvas.pack(fill = X)
-
 
 text_scroll = Scrollbar(canvas)
 text_scroll.pack(side=RIGHT, fill = BOTH)
@@ -80,20 +119,29 @@ text_scroll.pack(side=RIGHT, fill = BOTH)
 text_info = Text(canvas, yscrollcommand = text_scroll.set, wrap="none")
 text_info.config(bg='#362f2e', fg='#d2ded1', insertbackground='white')
 text_info.pack(fill=BOTH, side = RIGHT, expand= True)
+text_info.bind('<MouseWheel>', on_mousewheel)
 
 
 text_lineNum = Text(canvas, width=2, yscrollcommand = text_scroll.set, wrap="none")
 text_lineNum.config(bg='#362f2e', fg='#d2ded1', insertbackground='white')
 text_lineNum.pack(fill=BOTH, side = LEFT)
+text_lineNum.bind('<MouseWheel>', on_mousewheel)
 
 
 text_scroll.config(command=multipleview)
 
+scrollbarErrors = Scrollbar(root)
+scrollbarErrors.pack( side = RIGHT, fill = Y)
 
-text_output = Text(root, height=7)
+
+text_output = Text(root, yscrollcommand = scrollbarErrors.set, height=7)
 text_output.config(bg='#362f2e', fg='#1dd604')
 text_output.configure(state='disabled')
 text_output.pack(fill=BOTH, expand= True)
+
+
+scrollbarErrors.config(command = text_output.yview )
+
 
 menu_bar = Menu(root)
 
@@ -109,7 +157,8 @@ run_bar.add_command(label='Compile', command = comp)
 run_bar.add_command(label='Compile and Run', command = comp_run)
 menu_bar.add_cascade(label='Compile', menu = run_bar)
 
-
+    
+line_num = "1"
 
 def line_jump_checker():
     current_lines = 1;
@@ -131,6 +180,8 @@ def line_jump_checker():
                 current_lines += 1
                 line_num += "\n"+str(current_lines)
                 update_lineNums(line_num)
+            text_lineNum.yview(END)
+            text_info.yview(END)
                 
         elif len(current_text) > len(text_info.get('1.0', 'end-1c')):
             current_text_temp = text_info.get('1.0', 'end-1c');
@@ -145,10 +196,11 @@ def line_jump_checker():
                 current_lines -= 1
                 line_num = line_num[0:len(line_num)-2]
                 update_lineNums(line_num)
-                
-                
-        print(line_num)
-        time.sleep(0.01)
+            text_lineNum.yview(END)
+            text_info.yview(END)
+
+        
+        time.sleep(0.001)
     
 
 def update_lineNums(line_num):
@@ -157,6 +209,35 @@ def update_lineNums(line_num):
     text_lineNum.delete('1.0', END)
     text_lineNum.insert('1.0', line_num)
     text_lineNum.configure(state='disabled')
+
+
+
+def errorFound(lineErrors):
+    global errors_names
+    current_program = text_info.get('1.0', 'end-1c')
+    line_counter = 0
+    errors_names =[]
+    found = False
+    temp = text_lineNum.get('1.0', 'end-1c')
+    for j in range(len(current_program)):
+        
+        if current_program[j] == "\n":
+                line_counter += 1
+                
+        if j in lineErrors:
+            
+            text_lineNum.tag_add("E"+str(line_counter+1), str(line_counter+1)+".0", str(line_counter+1)+".5")
+            text_lineNum.tag_config("E"+str(line_counter+1), background= "red", foreground= "white")
+            errors_names += ["E"+str(line_counter+1)]
+            found = True
+
+    return found
+                
+            
+                
+    print(errors_names)
+
+
 
 
 line_thread = threading.Thread(target=line_jump_checker)
